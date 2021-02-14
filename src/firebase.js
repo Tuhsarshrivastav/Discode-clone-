3870ac75bbb0727b3a1fc3e93541c8a03f9bfa77
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2021-02-14 08:51:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-2rWRIkdd5Gr-9aYIS7p-z90vjvAPTKw",
-  authDomain: "discord-clone-a2d7b.firebaseapp.com",
-  projectId: "discord-clone-a2d7b",
-  storageBucket: "discord-clone-a2d7b.appspot.com",
-  messagingSenderId: "148086133792",
-  appId: "1:148086133792:web:93294a12602a9c5b8b5845",
-  measurementId: "G-27B2MB9LHF",
+//copy your firebase config cdn link 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
